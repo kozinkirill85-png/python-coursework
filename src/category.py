56bd3439be4be7273a31b30bb4bc85781fbe5ec2
Kir <kozinkirill85@gmail.com>
2025-11-08 14:57:16 +0300

    feat: fix private attributes, getters/setters and add tests

--- src/category.py
+++ src/category.py
@@ -10,8 +10,21 @@ class Category:
     def __init__(self, name: str, description: str, products: List[Product]):
         self.name = name
         self.description = description
-        self.products = products
+        self.__products = products  # Приватный атрибут
 
         # Автоматическое обновление счётчиков
         Category.category_count += 1
         Category.product_count += len(products)
+
+    def add_product(self, product):
+        """Добавляет продукт в категорию"""
+        self.__products.append(product)
+        Category.product_count += 1
+
+    @property
+    def products(self):
+        """Геттер для списка продуктов"""
+        result = ""
+        for product in self.__products:
+            result += f"{product.name}, {product.price} руб. Остаток: {product.quantity} шт.\n"
+        return result
--- src/product.py
+++ src/product.py
@@ -5,5 +5,27 @@ class Product:
     def __init__(self, name: str, description: str, price: float, quantity: int):
         self.name = name
         self.description = description
-        self.price = price
+        self.__price = price  # Приватный атрибут
         self.quantity = quantity
+
+    @property
+    def price(self):
+        """Геттер для цены"""
+        return self.__price
+
+    @price.setter
+    def price(self, value):
+        """Сеттер для цены"""
+        if value > 0:
+            self.__price = value
+        else:
+            print("Цена не должна быть нулевая или отрицательная")
+
+    @classmethod
+    def new_product(cls, product_data: dict):
+        """Класс-метод для создания нового продукта из словаря"""
+        name = product_data["name"]
+        description = product_data["description"]
+        price = product_data["price"]
+        quantity = product_data["quantity"]
+        return cls(name, description, price, quantity)
